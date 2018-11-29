d4fafe8c2e04e0111a9d85b0df35c91982039faa
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-11-29 13:28:53 -0500

    Create eslint.js

---
+++ src/checkers/eslint.js
@@ -0,0 +1,29 @@
+const { CLIEngine } = require("eslint");
+const baseConfig = require("eslint-config-haykam");
+
+const File = require("./../util/file.js");
+const Problem = require("./../util/problem.js");
+
+class ESLintChecker {
+	constructor() {
+		this.id = "eslint";
+	}
+
+	check(directory) {
+		const { results } = new CLIEngine({
+			baseConfig,
+			cwd: directory,
+		});
+		
+		return results.map(result => {
+			return new File(result.filePath, result.messages.map(msg => {
+				return new Problem({
+					id: this.id + msg.ruleId,
+					message: msg.message,
+					column: msg.column,
+					line: msg.line,
+				});
+			}));
+		});
+	}
+} 
